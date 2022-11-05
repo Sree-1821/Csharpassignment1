e1fa74ab5f8b90bcab21acc57696be92ac89336a
Sree-1821 <115399036+Sree-1821@users.noreply.github.com>
2022-11-05 06:45:08 +0000

    Add files via upload

---
+++ Area and circumstance.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharpassignment1
+{
+    internal class Area_and_circumstance
+    {
+        public static void Main()
+        {
+            try
+            {
+                float PI = 3.141F;
+                float R;
+                Console.WriteLine("Enetr the Radius Value");
+                R = int.Parse(Console.ReadLine());
+                float circum = 2 * PI * R * R;
+                float area = PI * R * R;
+                Console.WriteLine("Circumfrence = {0} and Area = {1}", circum, area);
+            }
+            catch (Exception Error)
+            {
+                Console.WriteLine(Error.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("The Value is Final");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+
+    
